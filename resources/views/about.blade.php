48fff628f0d0fd57e90847c838c671e27a8e7051
Laptop Alnino <alnino007@gmail.com>
2023-10-30 17:22:42 +0700

    Layouting dengan laravel

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('main_layout.main')
+
+@section('header')
+<h1>Halaman About</h1>
+<img src="images/dio.png" alt="alnino dio putera" width="100" height="100">
+<h3>Alnino Dio putera</h3>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('main_layout.main')
+
+@section('header')
+<h1>Halaman Home</h1>
+@endsection
