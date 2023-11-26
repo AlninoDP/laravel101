d1c03152d95ad28695461d12e0dec3cdb496665a
AlninoDP <alninodp@gmail.com>
2023-11-26 14:49:43 +0700

    tampilkan data dari database dengan table

--- resources/views/data_mhs.blade.php
+++ resources/views/data_mhs.blade.php
@@ -3,3 +3,39 @@
 @section('header')
     <h1>Halaman Data</h1>
 @endsection
+
+@section('body')
+<style>
+    
+table, th, td {
+  border: 2px solid black;
+}
+
+td {
+    font-size: 14px;
+} 
+
+</style>
+<table style="text-align:center;">
+    <thead>
+        <tr>
+            <th>Nim</th>
+            <th>Nama Mahasiswa</th>
+            <th>Kode Matkul</th>
+            <th>Mata Kuliah</th>
+            <th>Nilai</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($nilais as $dataNilai)
+            <tr>
+                <td>{{ $dataNilai->nim }}</td> 
+                <td>{{ $dataNilai->mahasiswa->nama }}</td>
+                <td>{{ $dataNilai->kode_matkul }}</td>
+                <td>{{ $dataNilai->matakuliah->nama_matkul }}</td>
+                <td style="font-weight: bold; color: {{ $dataNilai->nilai < 70 ? 'red' : 'black' }}">{{ $dataNilai->nilai }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+@endsection
