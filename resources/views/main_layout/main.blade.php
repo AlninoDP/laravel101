f09071a87fd4dd0ffcc2246d32cffe33c72e1e41
AlninoDP <alninodp@gmail.com>
2023-11-26 14:49:25 +0700

    tambah section body di main layout

--- resources/views/main_layout/main.blade.php
+++ resources/views/main_layout/main.blade.php
@@ -19,6 +19,10 @@
         @yield('header')
     </div>
 
+    <div class="container mt-4">
+        @yield('body')
+    </div>
+
  
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">   
